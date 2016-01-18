e71aadae92c71e02133be2d85fc8468ed724e177
Eduardo <lebas66@gmail.com>
2016-01-18 02:35:23 -0200

    [add] suplit_bank supplier

---
+++ lib/iugu/bank.rb
@@ -0,0 +1,9 @@
+module Iugu
+  class Bank < APIResource
+  	include Iugu::APIFetch
+
+  	def withdrawals
+  		Iugu::Factory.create_from_response(object_type, APIRequest.request("GET", "#{url}/v1/transfers"))
+    end
+  end
+end
